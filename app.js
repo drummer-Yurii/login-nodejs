5b2a9510f85959e6758616097aaebba17daf327a
Yurii Rybachok <drummer2009@ukr.net>
2023-11-19 11:05:25 +0200

    setup middleware

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+const hbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
@@ -11,3 +11,15 @@ mongoose.connect("mongodb://localhost:27017/node-auth", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+//Middleware
+app.engine('hbs', hbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: "verygoodsecret",
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
