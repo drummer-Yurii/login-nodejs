51601604656dcfea5c9201c1a35fc272e16cfdad
Yurii Rybachok <drummer2009@ukr.net>
2023-11-19 11:52:39 +0200

    check user authenticated

--- app.js
+++ app.js
@@ -50,3 +50,16 @@ passport.deserializeUser(function (id, done) {
         done(err, user);
     });
 });
+
+passport.use(new localStrategy(function (username, password, done) {
+    User.findOne({ username: username}, function (err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: 'Incorrect username.'});
+
+        bcrypt.compare(password, user.password, function (err, res) {
+            if (err) return done(err);
+            if (res === false) return done(null, false, { message: 'Incorrect password.'});
+            return done(null, user);
+        });
+    });
+}));
